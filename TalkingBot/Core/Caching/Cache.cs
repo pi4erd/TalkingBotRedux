3950162ea17b9b8f50171681137edadead7521a8
pi4erd <piuuttri@gmail.com>
2025-01-26 14:09:31 +0100

    feat: Added cacher

---
+++ TalkingBot/Core/Caching/Cache.cs
@@ -0,0 +1,50 @@
+using Discord;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace TalkingBot.Core.Caching;
+
+// NOTE: This is a service
+public class Cache<T>(ILogger<Cache<T>> logger) {
+    private string typename = typeof(T).Name;
+    // TODO: Allow setting cache directory in a config
+    private string cacheDirectory = Directory.GetCurrentDirectory();
+
+    public void SaveCached(T[] cache) {
+        string json = JsonConvert.SerializeObject(cache);
+
+        string filename = $"cache_{typename}.json";
+        string dir = cacheDirectory + "/Cache/";
+        
+        if(!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+
+        logger.LogInformation("Saved cache for {}.", typename);
+
+        using(StreamWriter sw = new StreamWriter(dir + filename)) {
+            sw.Write(json);
+        }
+    }
+    
+    public T[]? LoadCached() {
+        string filename = $"cache_{typename}.json";
+        string dir = cacheDirectory + "/Cache/";
+
+        string json = "";
+
+        try {
+            using StreamReader sr = new(dir + filename);
+
+            json = sr.ReadToEnd();
+        } catch(IOException) {
+            if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+            File.Create(dir + filename);
+            return null;
+        }
+
+        logger.LogInformation("Loaded cache for {}.", typename);
+
+        var result = JsonConvert.DeserializeObject<T[]>(json);
+
+        return result;
+    }
+}
