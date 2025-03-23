5b96a10165ab015eebceae341ed33a6f704c5521
pi4erd <piuuttri@gmail.com>
2025-03-23 15:21:33 +0100

    feat: On voice empty, disconnect

--- TalkingBot/Services/AudioEventListener.cs
+++ TalkingBot/Services/AudioEventListener.cs
@@ -3,6 +3,7 @@ using Discord.WebSocket;
 using Lavalink4NET;
 using Lavalink4NET.Events.Players;
 using Lavalink4NET.Extensions;
+using Lavalink4NET.Players;
 using Lavalink4NET.Protocol.Payloads.Events;
 using Microsoft.Extensions.Logging;
 using TalkingBot.Core;
@@ -28,6 +29,43 @@ public class AudioEventListener : IDisposable {
         _audioService.TrackStarted   += OnTrackStarted;
         _audioService.TrackEnded     += OnTrackEnd;
         _audioService.TrackException += OnTrackException;
+
+        _client.UserVoiceStateUpdated += UserVoiceStateUpdated;
+    }
+
+    public async Task UserVoiceStateUpdated(
+        SocketUser user,
+        SocketVoiceState prevState,
+        SocketVoiceState newState
+    ) {
+        if(user.IsBot) {
+            return;
+        }
+
+        // Ignore join
+
+        SocketGuild? guild = prevState.VoiceChannel?.Guild;
+        if(guild is null) {
+            return;
+        }
+        
+        DiscordSocketClient? client = _client.GetShardFor(guild);
+
+        if(client is null) {
+            _logger.LogWarning("Shard found null when user state left: {}.", guild);
+            return;
+        }
+
+        if(!_audioService.Players.TryGetPlayer(guild.Id, out LavalinkPlayer? player)) {
+            return;
+        }
+        
+        if(player!.VoiceChannelId == prevState.VoiceChannel!.Id &&
+            prevState.VoiceChannel.ConnectedUsers.Count <= 1)
+        {
+            await player!.DisconnectAsync();
+            await client.SetActivityAsync(new Game("Nothing", ActivityType.Listening));
+        }
     }
 
     public async Task OnTrackStarted(object sender, TrackStartedEventArgs args) {
